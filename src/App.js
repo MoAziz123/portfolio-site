c6a909f4e4e1a53e40bd7eabe273cf559d3f0417
Mo <mohammed4685@outlook.com>
2020-10-10 21:14:03 +0100

    browserrouter changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //modules
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 //css
 import logo from './logo.svg';
 import './App.css';
