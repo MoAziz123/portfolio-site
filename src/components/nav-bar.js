97b4eb156f20050e1e0b945b98ed2cc38818e196
Mo <mohammed4685@outlook.com>
2020-10-11 03:08:01 +0100

    changes made to hash

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -38,7 +38,6 @@ export default class NavBar extends React.Component{
         let elements = document.getElementsByClassName("page-button")
         for(let i = 0; i < elements.length; i++)
         {
-            console.log(window.location.hash)
             if(window.location.hash === elements[i].getAttribute("name"))
             {
                 
@@ -63,19 +62,19 @@ export default class NavBar extends React.Component{
                         <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("home")}} onClick={(e)=>{this.handleRedirect("/")}} className="fa-2x fa-home"/>
                     </div>
                     <div name="#/about" className="page-button">
-                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("about")}} onClick={(e)=>{this.handleRedirect("/about")}} className="fa-2x fa-user page-button icon"/>
+                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("about")}} onClick={(e)=>{if(window.location.hash !== "#/about")this.handleRedirect("/about")}} className="fa-2x fa-user page-button icon"/>
                     </div>
                     <div name="#/contact" className="page-button">
-                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("contact")}} onClick={(e)=>{this.handleRedirect("contact")}} className="fa-2x fa-address-book page-button icon" href="about.html" />
+                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("contact")}} onClick={(e)=>{if(window.location.hash !== "#/contact")this.handleRedirect("contact")}} className="fa-2x fa-address-book page-button icon" href="about.html" />
                     </div>
                     <div name="#/testimonial" className="page-button">
-                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("testimonial")}} onClick={(e)=>{this.handleRedirect("testimonial")}} className="fa-2x fa-commenting page-button icon" />
+                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("testimonial")}} onClick={(e)=>{if(window.location.hash !== "#/testimonial")this.handleRedirect("testimonial")}} className="fa-2x fa-commenting page-button icon" />
                     </div>
                     <div name="#/projects" className="page-button">
-                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseEnter={(e)=>{this.handleHover("projects")}} onClick={(e)=>{this.handleRedirect("projects")}} className="fa-2x fa-tasks page-button icon" />
+                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseEnter={(e)=>{this.handleHover("projects")}} onClick={(e)=>{if(window.location.hash !== "#/projects")this.handleRedirect("projects")}} className="fa-2x fa-tasks page-button icon" />
                     </div>
                     <div name="#/cv" className="page-button">
-                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("cv")}} onClick={(e)=>{this.handleRedirect("cv")}} className="fa-2x fa-newspaper-o fapage-button icon"/>
+                        <FontAwesome onMouseLeave={(e)=>{this.handleHoverOut()}} onMouseOver={(e)=>{this.handleHover("cv")}} onClick={(e)=>{if(window.location.hash !== "#/cv")this.handleRedirect("cv")}} className="fa-2x fa-newspaper-o fapage-button icon"/>
                     </div>
                 </div>
                 <p className="link-text">{this.state.text}</p>
